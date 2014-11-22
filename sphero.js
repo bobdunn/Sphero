f7bc424c449736a4e71b13f09fdd35d42061531a
Bob Dunn <rpdunn@gmail.com>
2014-11-21 23:47:40 -0700

    Fixing some dumb errors

--- sphero.js
+++ sphero.js
@@ -1,9 +1,11 @@
+var Cylon = require('cylon');
+var math = require('./math');
+var _ = require('underscore');
+
 var sphero = function () {
 
     var self = {};
 
-    var Cylon = require('cylon');
-    var math = require('./math');
     var targetPosition = {x: 0, y: 0};
     var currentPosition = {x: 0, y: 0};
     var dataCallbacks = [];
@@ -15,6 +17,9 @@ var sphero = function () {
     self.setPositionCallback = function (callback) {
         dataCallbacks.push(callback);
     };
+    self.moveTo = function (target) {
+        targetPosition = target;
+    };
 
     var cylon = Cylon.robot({
         connection: {name: 'sphero', adaptor: 'sphero', port: 'COM6'},
@@ -22,21 +27,14 @@ var sphero = function () {
 
         work: function (my) {
 
-            var moveTo = function (target) {
-                console.log('New target: ' + target);
-                targetPosition = target;
-            };
             my.sphero.detectCollisions();
             my.sphero.readLocator();
             my.sphero.configureLocator(1, 0, 0, 0);
 
-            console.log("Connected!");
-
             var moveTowardTarget = function () {
-                console.log(currentPosition);
                 var polar = math.getPolar(targetPosition, currentPosition);
-                var speed = Math.min((polar.distance * 25).toFixed(), 255);
-                console.log({polar: polar, speed: speed});
+                var speed = Math.min((polar.r * 25).toFixed(), 255);
+                console.log({polar:polar,speed:speed});
                 my.sphero.roll(speed, polar.theta);
             };
 
@@ -46,10 +44,10 @@ var sphero = function () {
                     position: {x: data[0], y: data[1]},
                     velocity: {x: data[2], y: data[3], speed: data[4]}
                 };
-                console.log(callbackData);
-                for (var i = 0; i < dataCallbacks.length; i++) {
-                    dataCallbacks[i](callbackData);
-                }
+                _.each(dataCallbacks, function (callback) {
+                    callback(callbackData);
+                });
+
                 moveTowardTarget();
             });
         }
